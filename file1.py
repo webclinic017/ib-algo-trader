21bb6679909dd0629df99b356f00c0ff2c3a2daa
bricksz <36253313+bricksz@users.noreply.github.com>
2018-05-17 23:27:28 -0400

    minor

---
+++ file1.py
@@ -0,0 +1,2 @@
+import scrappy_ticker_api as scr
+scr.grab('http://www.bloomberg.com/quote/SPX:IND', 2)
--- scrappy_ticker_api.py
+++ scrappy_ticker_api.py
@@ -4,25 +4,35 @@ import csv
 from datetime import datetime
 import time
 
-quote_page ='http://www.bloomberg.com/quote/SPX:IND'        # decleared url
-page = urllib.request.urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')                   # parse html using BeautifulSoup, store in soup.
+def grab(a,y=None):
+    quote_page =a      # decleared url
+    page = urllib.request.urlopen(quote_page)
+    soup = BeautifulSoup(page, 'html.parser')                   # parse html using BeautifulSoup, store in soup.
 
-name_box = soup.find('h1', attrs={'class':'name'})          # class name is unique, can query <div class='name'>
+    '''
+    ticker_box = soup.find('div', attrs={'class', 'ticker'})
+    ticker = ticker_box.text.strip()
+    print(ticker)
+    '''
+
+    name_box = soup.find('h1', attrs={'class':'name'})          # class name is unique, can query <div class='name'>
                                                             # remove <div> of the name and get its value
-name = name_box.text.strip()                                # strip() used to remove starting and trailing, i.e. spaces.
+    name = name_box.text.strip()                                # strip() used to remove starting and trailing, i.e. spaces.
                                                             #
-print(name)
+    print(name)
 
-price_box = soup.find('div', attrs={'class':'price'})
-price = price_box.text
+    price_box = soup.find('div', attrs={'class':'price'})
+    price = price_box.text
 
-print(price)
-
-x = 0
-while x < 20:
-    with open('index.csv', 'a') as csv_file:                    # where 'a' appends to the end of the file.
-        writer = csv.writer(csv_file)
-        writer.writerow([name, price, datetime.now()])
-    time.sleep(5)
-    x += 1
+    print(price)
+    x=0
+    if y is None:
+        y = 20
+    else:
+        y = y
+    while x < y:
+        with open('index.csv', 'a') as csv_file:                    # where 'a' appends to the end of the file.
+            writer = csv.writer(csv_file)
+            writer.writerow([name, price, datetime.now()])
+        time.sleep(5)
+        x += 1
